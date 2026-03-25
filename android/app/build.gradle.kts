25aed36edb747404b1117aaeadfd833313ef91d3
niwooie223 <olegreut@gmail.com>
2026-03-25 08:37:33 -0700

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,7 +18,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.cclbcorps.traccar.client"
+    namespace = "com.cclbcorps.gps"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -32,7 +32,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.cclbcorps.traccar.client"
+        applicationId = "com.cclbcorps.gps"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
